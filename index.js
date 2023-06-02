ce4c7fd2f2ef27506943949658280865157da725
Eric Harrison <me@ericharrison.info>
2023-06-02 18:33:02 -0400

    Change statistical probability of a bad answer

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ async function runCompletion(input) {
 	try {
 		let last = lastAnswer();
 		conversations.push({ role: 'system', 'content': completion.data.choices[0].message.content });
-		if ( Math.random() > .5 && last ) {
+		if ( Math.random() > .3 && last ) {
 			return last.content;
 		} else {
 			return completion.data.choices[0].message.content;
